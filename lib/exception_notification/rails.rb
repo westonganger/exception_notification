14ad289b0419ff1aca960cccfb00fa625b91cfa6
Martin Hong <hongzeqin@gmail.com>
2017-03-04 14:18:17 +0800

    the engine auto the error grouping cache to Rails.cache

--- lib/exception_notification/rails.rb
+++ lib/exception_notification/rails.rb
@@ -2,6 +2,7 @@ module ExceptionNotification
   class Engine < ::Rails::Engine
     config.exception_notification = ExceptionNotifier
     config.exception_notification.logger = Rails.logger
+    config.exception_notification.error_grouping_cache = Rails.cache
 
     config.app_middleware.use ExceptionNotification::Rack
   end
