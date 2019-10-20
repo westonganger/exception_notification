835833578af54d6900e476f38615ccc917a2bc78
elengine <info.el.engine@gmail.com>
2019-10-20 22:47:54 +0900

    Change test name.

--- test/exception_notifier_test.rb
+++ test/exception_notifier_test.rb
@@ -137,7 +137,7 @@ class ExceptionNotifierTest < ActiveSupport::TestCase
     assert_equal @notifier_calls, 1
   end
 
-  test 'should not send notification if extended module at singleton class one of ignored exceptions' do
+  test 'should not send notification if extended module one of ignored exceptions' do
     ExceptionNotifier.register_exception_notifier(:test, @test_notifier)
 
     module StandardErrorModule; end
